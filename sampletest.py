c28bd08efe618babe27a75628eba420425fba9bf
chandrashekhar-tech <90205119+chandrashekhar-tech@users.noreply.github.com>
2025-01-30 23:18:48 +0530

    Create sampletest.py

---
+++ sampletest.py
@@ -0,0 +1,6 @@
+# This is a simple Python program
+print("Hello, World!")
+
+# Adding some more text
+print("Welcome to the world of Python programming!")
+print("Let's create something amazing together.")
